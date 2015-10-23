d26d79fe7543d32dd72bd217a928d686fe283233
Travis Hoki <travis.hoki@gmail.com>
2015-10-23 16:10:53 -0600

    fix arrows, add js to git ignore

--- js/dest/overstock-shortcodes.js
+++ js/dest/overstock-shortcodes.js
@@ -13,7 +13,7 @@ $ostk_jQuery(window).load(function() {
 		_this.children('.ostk-flexslider').flexslider({
 			animation: "slide",
 			controlNav: "thumbnails",
-			customDirectionNav: _this.find(".custom-navigation").children("a"),
+			customDirectionNav: _this.find(".custom-navigation").children(".arrow"),
 			controlsContainer: _this.find(".custom-controls-container"),
 			touch: true,
 			slideshow: false,
