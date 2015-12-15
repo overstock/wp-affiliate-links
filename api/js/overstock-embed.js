772eb9f59f03d7b28d30ce7b8a7cc72a80b0ca22
Travis Hoki <travis.hoki@gmail.com>
2015-12-15 15:41:50 -0700

    fix callback for getJSON

--- api/js/overstock-embed.js
+++ api/js/overstock-embed.js
@@ -1541,7 +1541,7 @@ function ostk_Plugin(){
 	this.getPatterns = function(){
 		var _this = this;
 		var json_url = ostk_api_url + "patterns.json";
-		json_url += "jsoncallback=?";
+		json_url += "&jsoncallback=?";
 		$ostk_jQuery.getJSON(json_url, function(ostk_patterns) {
 			_this.ostk_patterns = ostk_patterns;
 			_this.get_elements();
