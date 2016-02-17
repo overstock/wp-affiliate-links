0fee1319d78d77fafd8ddab75282983b9bfe69f8
Travis Hoki <travis.hoki@gmail.com>
2016-02-17 16:32:38 -0700

    self calling function for loading api js

--- api/dist/overstock-embed.js
+++ api/dist/overstock-embed.js
@@ -1292,59 +1292,67 @@ function ostk_array_to_list_string(a, deliminator){
   }//switch
 }//ostk_array_to_and_list_string
 
-// Only load the plugin if it hasn't already been loaded. 
-// Widget embeds might include the script tag multiple times.
-if(typeof(ostk_plugin) == 'undefined'){
-	var ostk_developerId = null;
+function ostk_script_init(){
+	console.log('%c========== ostk_script_init ==========', 'font-size: 16px');
+	// Only load the plugin if it hasn't already been loaded. 
+	// Widget embeds might include the script tag multiple times.
+	if(typeof(ostk_plugin) == 'undefined'){
+		var ostk_developerId = null;
 
-	if(!ostk_isset(ostk_clickPlatform)){
-		var ostk_clickPlatform = 'embed';
-	}
-
-	var ostk_clickurl = window.location.href;
-
-	var scripts = document.getElementsByTagName('script');
-
-	for(var i = 0 ; i < scripts.length ; i++){
-		if(
-		//TODO: overstock-embed needs to be deprecated at some point
-		scripts[i].src.indexOf("overstock-embed") > -1 ||
-		scripts[i].src.indexOf("affiliate-link-plugin/api") > -1
-		){
-			var id_value = ostk_searchURLForParam(scripts[i].src, 'id');
-			if(id_value){
-				ostk_developerId = id_value;
-				break;
-			}
-		}	
-	}//for
-
-	var ostk_url = 'https://api.overstock.com';
-	if(
-		typeof(os) !== 'undefined' && 
-		typeof(os.Otags) !== 'undefined' && 
-		typeof(os.Otags.api_url) !== 'undefined'
-	){
-		ostk_url = os.Otags.api_url;
-	}
-	var event_list = [
-		{
-			'event': 'flash_deals',
-			'url': ostk_url+'/ads/products/deals?developerid='+ostk_developerId+'&sort=lowest_price'
-		},
-		{
-			'event': 'promotions',
-			'url': ostk_url+'/ads/sales?developerid='+ostk_developerId+'&sale_type=promotion'
-		},
-		{
-			'event': 'sales',
-			'url': ostk_url+'/ads/sales?developerid='+ostk_developerId+'&sale_type=sale'
+		if(!ostk_isset(ostk_clickPlatform)){
+			var ostk_clickPlatform = 'embed';
 		}
-	];
 
-	var ostk_plugin = new ostk_Plugin();
-}
+		var ostk_clickurl = window.location.href;
 
+		var scripts = document.getElementsByTagName('script');
+		// console.log('scripts.length: ' + scripts.length);
+		for(var i = 0 ; i < scripts.length ; i++){
+			// console.log(scripts[i].src);
+			if(
+			//TODO: overstock-embed needs to be deprecated at some point
+			scripts[i].src.indexOf("overstock-embed") > -1 ||
+			scripts[i].src.indexOf("affiliate-link-plugin/api") > -1
+			){
+				var id_value = ostk_searchURLForParam(scripts[i].src, 'id');
+				if(id_value){
+					ostk_developerId = id_value;
+					console.log('found api script with id');
+					break;
+				}
+			}	
+		}//for
+
+		console.log('ostk_developerId: ' + ostk_developerId);
+
+		var ostk_url = 'https://api.overstock.com';
+		if(
+			typeof(os) !== 'undefined' && 
+			typeof(os.Otags) !== 'undefined' && 
+			typeof(os.Otags.api_url) !== 'undefined'
+		){
+			ostk_url = os.Otags.api_url;
+		}
+		var event_list = [
+			{
+				'event': 'flash_deals',
+				'url': ostk_url+'/ads/products/deals?developerid='+ostk_developerId+'&sort=lowest_price'
+			},
+			{
+				'event': 'promotions',
+				'url': ostk_url+'/ads/sales?developerid='+ostk_developerId+'&sale_type=promotion'
+			},
+			{
+				'event': 'sales',
+				'url': ostk_url+'/ads/sales?developerid='+ostk_developerId+'&sale_type=sale'
+			}
+		];
+
+		var ostk_plugin = new ostk_Plugin();
+	}
+}//ostk_load
+
+ostk_script_init();
 /*
 ==================== Multi Product Data ====================
 
--- api/js/src/overstock-embed.js
+++ api/js/src/overstock-embed.js
@@ -1,52 +1,55 @@
-// Only load the plugin if it hasn't already been loaded. 
-// Widget embeds might include the script tag multiple times.
-if(typeof(ostk_plugin) == 'undefined'){
-	var ostk_developerId = null;
+function ostk_script_init(){
+	// Only load the plugin if it hasn't already been loaded. 
+	// Widget embeds might include the script tag multiple times.
+	if(typeof(ostk_plugin) == 'undefined'){
+		var ostk_developerId = null;
 
-	if(!ostk_isset(ostk_clickPlatform)){
-		var ostk_clickPlatform = 'embed';
-	}
-
-	var ostk_clickurl = window.location.href;
-
-	var scripts = document.getElementsByTagName('script');
-
-	for(var i = 0 ; i < scripts.length ; i++){
-		if(
-		//TODO: overstock-embed needs to be deprecated at some point
-		scripts[i].src.indexOf("overstock-embed") > -1 ||
-		scripts[i].src.indexOf("affiliate-link-plugin/api") > -1
-		){
-			var id_value = ostk_searchURLForParam(scripts[i].src, 'id');
-			if(id_value){
-				ostk_developerId = id_value;
-				break;
-			}
-		}	
-	}//for
-
-	var ostk_url = 'https://api.overstock.com';
-	if(
-		typeof(os) !== 'undefined' && 
-		typeof(os.Otags) !== 'undefined' && 
-		typeof(os.Otags.api_url) !== 'undefined'
-	){
-		ostk_url = os.Otags.api_url;
-	}
-	var event_list = [
-		{
-			'event': 'flash_deals',
-			'url': ostk_url+'/ads/products/deals?developerid='+ostk_developerId+'&sort=lowest_price'
-		},
-		{
-			'event': 'promotions',
-			'url': ostk_url+'/ads/sales?developerid='+ostk_developerId+'&sale_type=promotion'
-		},
-		{
-			'event': 'sales',
-			'url': ostk_url+'/ads/sales?developerid='+ostk_developerId+'&sale_type=sale'
+		if(!ostk_isset(ostk_clickPlatform)){
+			var ostk_clickPlatform = 'embed';
 		}
-	];
 
-	var ostk_plugin = new ostk_Plugin();
-}
+		var ostk_clickurl = window.location.href;
+
+		var scripts = document.getElementsByTagName('script');
+		for(var i = 0 ; i < scripts.length ; i++){
+			if(
+			//TODO: overstock-embed needs to be deprecated at some point
+			scripts[i].src.indexOf("overstock-embed") > -1 ||
+			scripts[i].src.indexOf("affiliate-link-plugin/api") > -1
+			){
+				var id_value = ostk_searchURLForParam(scripts[i].src, 'id');
+				if(id_value){
+					ostk_developerId = id_value;
+					break;
+				}
+			}	
+		}//for
+
+		var ostk_url = 'https://api.overstock.com';
+		if(
+			typeof(os) !== 'undefined' && 
+			typeof(os.Otags) !== 'undefined' && 
+			typeof(os.Otags.api_url) !== 'undefined'
+		){
+			ostk_url = os.Otags.api_url;
+		}
+		var event_list = [
+			{
+				'event': 'flash_deals',
+				'url': ostk_url+'/ads/products/deals?developerid='+ostk_developerId+'&sort=lowest_price'
+			},
+			{
+				'event': 'promotions',
+				'url': ostk_url+'/ads/sales?developerid='+ostk_developerId+'&sale_type=promotion'
+			},
+			{
+				'event': 'sales',
+				'url': ostk_url+'/ads/sales?developerid='+ostk_developerId+'&sale_type=sale'
+			}
+		];
+
+		var ostk_plugin = new ostk_Plugin();
+	}
+}//ostk_load
+
+ostk_script_init();
